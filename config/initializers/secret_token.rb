ba42135f7e29e10bf94509dfe24ef8873b9c0fe6
dkelman <dkelman@crossrider.com>
2013-08-10 23:59:17 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kelman::Application.config.secret_token = 'a07023cce251ad4fddeb07bc0e59da581edc094d44a62990863e53d2e300dd14d102df974f0267055c3fd2529512b68a6680d24e9e58a594700bf7b241afa149'
